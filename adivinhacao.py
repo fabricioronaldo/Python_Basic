abc916322150a03c4a6e89689b8438233d5c6b2b
Fabricio Ronaldo <fabriciorvelasco@gmail.com>
2021-09-04 22:33:14 -0300

    Inclusão de condicional para verificação do chute

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,10 +7,19 @@ number_secret = 42
 kick = int(input("Digite seu Número:"))
 print("O número digitado foi: ", kick)
 
-if (number_secret == kick):
+gotItRight = kick == number_secret
+bigger = number_secret > kick
+smaller = number_secret < kick
+
+
+if (gotItRight):
     print("Você acertou!!!")
 else:
-    print("Você Errou!!!")
+    if(bigger):
+
+        print("Você Errou!!! O Seu chute foi menor que o número secreto.")
+    elif(smaller):
+        print("Você Errou!!! O seu chute foi maior que o número secreto.")
 
 
 print("Fim do Jogo")
